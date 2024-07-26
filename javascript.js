8ecee5263b0e22802b4a148dea2f09d0548073be
jingyima <jinma717@gmail.com>
2024-07-26 14:50:01 -0400

    Whole feature complete

--- javascript.js
+++ javascript.js
@@ -2,15 +2,26 @@
 //the container that all the small grids will go into 
 const container = document.querySelector(".container");
 
-let gridCount = 16;
 let canvasSize = container.clientWidth;
 
 console.log(canvasSize);
 
+
+
+
 function createGrid() {
+    
+    container.innerHTML = '';
+    const input = document.querySelector("#sizeEntry");
+
+    let gridCount = input.value || 16;
+
     let gridSize = canvasSize/gridCount
 
-    console.log(gridSize);
+    if(isNaN(gridCount) || gridCount <= 0 || gridCount > 100) {
+        alert("Invalid grid size.");
+        return;
+    }
 
     for(i=0; i<gridCount*gridCount; i++) {
         const grid = document.createElement('div');
@@ -18,7 +29,24 @@ function createGrid() {
         grid.style.width = gridSize + 'px';
         grid.style.height = gridSize + 'px';
         container.appendChild(grid);
+
+        function draw(e){
+            const color = document.querySelector("#colorPicker")
+            e.target.style.backgroundColor = color.value;
+            }
+        
+        grid.addEventListener("mousemove", draw)
     }
+  
 }
 
 createGrid();
+
+const changeBtn = document.querySelector("button");
+changeBtn.addEventListener("click", createGrid);
+
+
+
+
+
+
